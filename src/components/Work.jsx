390cd0ed4dd4351fee5ececcc45f011686cf876d
ashiyaman <ashiya1005@gmail.com>
2025-05-06 14:18:07 +0530

    feat: Add projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,38 +1,104 @@
+import "./Pages.css";
+import "./Work.css";
+
 const Work = () => {
-    return (
-        <section className="hero">
-            <h2>Projects</h2>
-            <div>
-                <a href='https://sprout-nest-fe.vercel.app/'>
-                    <img />
-                    <div>
-                        <h4>Project Title</h4>
-                        <p>Description</p>
-                        <p>Technology used</p>
-                    </div>
-                </a>
-                <a href='https://dish-deck-fe.vercel.app/'>
-                    <img />
-                    <div>
-                        <h4>Project Title</h4>
-                        <p>Description</p>
-                        <p>Technology used</p>
-                    </div>
-                </a>
-                <a ref='https://eventura-fe.vercel.app/'>
-                    <img />
-                    <div>
-                        <h4>Project Title</h4>
-                        <p>Description</p>
-                        <p>Technology used</p>
-                    </div>
-                </a>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="hero">
+      <h3>Projects</h3>
+      <div className="work-section">
+        <div href="https://sprout-nest-fe.vercel.app/" className="work">
+          <img src="../../src/assets/sproutnest.png" />
+          <div className="work-details">
+            <div className="work-header">
+                <div>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>                
+                <h4>Plant Store</h4>
+            </div>            
+            <code>
+              <p>const project = {'{'}</p>
+              <p>Name: 'Sproutnest App',</p>
+              <p>
+                Tools: ['React JS', 'Redux Thunk', 'Mongo DB', 'Express', 'Node
+                JS', 'Bootstrap']
+              </p>
+              <p>
+                Description: Sprout Nest is an online store for plants and
+                gardening products. I developed the front-end using React.js,
+                Bootstrap, Redux, and Redux Thunk, implementing features like
+                product browsing, filtering, cart, and checkout. I also built
+                the back-end with Node.js and Express, and connected it to the
+                front-end using Redux Thunk for API integration.
+                {'}'}
+              </p>
+            </code>
+          </div>
+        </div>
+        <div href="https://dish-deck-fe.vercel.app//" className="work">
+          <img src="../../src/assets/dishdeck.png" />
+          <div className="work-details">
+          <div className="work-header">
+                <div>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>                
+                <h4>Recipe Finder</h4>
+            </div>  
+            <code>
+              <p>const project = {'{'}</p>
+              <p>Name: 'Sproutnest App',</p>
+              <p>
+                Tools: ['React JS', 'Redux Thunk', 'Mongo DB', 'Express', 'Node
+                JS', 'Bootstrap']
+              </p>
+              <p>
+                Description: Sprout Nest is an online store for plants and
+                gardening products. I developed the front-end using React.js,
+                Bootstrap, Redux, and Redux Thunk, implementing features like
+                product browsing, filtering, cart, and checkout. I also built
+                the back-end with Node.js and Express, and connected it to the
+                front-end using Redux Thunk for API integration.
+                {'}'}
+              </p>
+            </code>
+          </div>
+        </div>
+        <div href="https://eventura-fe.vercel.app/" className="work">
+          <img src="../../src/assets/eventura.png" />
+          <div className="work-details">
+          <div className="work-header">
+                <div>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>                
+                <h4>Event Management</h4>
+            </div>  
+            <code>
+              <p>const project = {'{'}</p>
+              <p>Name: 'Sproutnest App',</p>
+              <p>
+                Tools: ['React JS', 'Redux Thunk', 'Mongo DB', 'Express', 'Node
+                JS', 'Bootstrap']
+              </p>
+              <p>
+                Description: Sprout Nest is an online store for plants and
+                gardening products. I developed the front-end using React.js,
+                Bootstrap, Redux, and Redux Thunk, implementing features like
+                product browsing, filtering, cart, and checkout. I also built
+                the back-end with Node.js and Express, and connected it to the
+                front-end using Redux Thunk for API integration.
+                {'}'}
+              </p>
+            </code>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Work;
-
-
-
